a139e38163592086710e1c6d512c629708718aac
rohang <rohangupta85@gmail.com>
2019-05-16 20:36:11 -0400

    added variables.tf

--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@ variable "availability_zone" {
 }
 variable "public_key_path" {
   description = "Public key path"
-  default = "~/.ssh/id_rsa.pub"
+  default = "~/.ssh/wordpress.pem"
 }
 variable "instance_ami" {
   description = "AMI for aws EC2 instance"
